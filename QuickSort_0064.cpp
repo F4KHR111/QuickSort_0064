5b031ba83bb96ccd183c776f762a3ceb89a7c1b3
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-03-29 19:07:42 +0700

    Penambahan Perkondisian If

--- QuickSort_0064.cpp
+++ QuickSort_0064.cpp
@@ -82,6 +82,10 @@ void display() {
 	cout << "\n------------------" << endl;
 	cout << "Sorted Array" << endl;
 	cout << "------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
 }
 
 int main()
