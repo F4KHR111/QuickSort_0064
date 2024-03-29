2895bb3421f850e0124653af4194517f485e9529
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-03-29 17:15:33 +0700

    Penambahan Prosedur Input

--- QuickSort_0064.cpp
+++ QuickSort_0064.cpp
@@ -7,6 +7,29 @@ int cmp_count = 0; //jumlah perbandingan
 int mov_count = 0; //jumlah pergerakan data
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukan Panjang Element Array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum Panjang Array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
+
 int main()
 {
  
