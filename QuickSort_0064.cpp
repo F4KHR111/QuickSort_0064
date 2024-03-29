70171896430b1355da63b8b205fd96eac9633528
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-03-29 19:01:35 +0700

    Penambahan Recrusive Call Untuk Mengurutkan Sub Larik Kanan

--- QuickSort_0064.cpp
+++ QuickSort_0064.cpp
@@ -74,6 +74,8 @@ void q_short(int low, int high) {
 	//recrusive call to sort the left sub array
 	q_short(low, j - 1);
 
+	//recrusive call to sort the right sub array
+	q_short(j + 1, high);
 }
 
 
